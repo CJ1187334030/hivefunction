5764677348546dc4a8566938fe68562fda11c6e5
chenj <1187334030@qq.com>
2021-01-16 23:13:35 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/udf/EventJsonUDTF.java
@@ -0,0 +1,25 @@
+package com.atguigu.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDTF;
+import org.apache.hadoop.hive.serde2.objectinspector.StructObjectInspector;
+
+public class EventJsonUDTF extends GenericUDTF {
+
+
+    @Override
+    public StructObjectInspector initialize(StructObjectInspector argOIs) throws UDFArgumentException {
+        return super.initialize(argOIs);
+    }
+
+    @Override
+    public void process(Object[] objects) throws HiveException {
+
+    }
+
+    @Override
+    public void close() throws HiveException {
+
+    }
+}
